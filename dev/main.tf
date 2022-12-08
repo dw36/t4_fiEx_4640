e9a806f9925d1a6dd8e0ae4903f0a50f7941fd30
dw36 <hin7788966558@gmail.com>
2022-12-08 00:07:37 +0000

    did terraform init

---
+++ dev/main.tf
@@ -0,0 +1,207 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    } 
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# configure ssh Key
+data "digitalocean_ssh_key" "ssh_key"{
+  name = var.ssh_key
+}
+
+#add new project
+data "digitalocean_project" "lab_project" {
+name = var.project
+}
+
+#Create a new tag
+resource "digitalocean_tag" "do_tag" {
+name = var.tag
+}
+
+
+#Create a new VPC
+resource "digitalocean_vpc" "web_vpc" {
+ name     = var.vpc
+ region   = var.region
+}
+
+
+# Create a bastion server
+resource "digitalocean_droplet" "bastion_dp" {
+  image    = var.rocky
+  size     = var.rsize
+  name     = "bastion-${var.region}"
+  tags   = [digitalocean_tag.do_tag.id]
+  region   = var.region
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+  vpc_uuid = digitalocean_vpc.web_vpc.id
+}
+
+# firewall for bastion server
+resource "digitalocean_firewall" "bastion_firewall" {
+  
+  #firewall name
+  name = "ssh-bastion-firewall"
+
+  # Droplets to apply the firewall to
+  droplet_ids = [digitalocean_droplet.bastion_dp.id]
+
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    destination_addresses = [digitalocean_vpc.web_vpc.ip_range]
+  }
+
+  outbound_rule {
+    protocol = "icmp"
+    destination_addresses = [digitalocean_vpc.web_vpc.ip_range]
+  }
+}
+
+# server.tf
+#########################################################################
+# Create a new Web Droplet in the var region
+resource "digitalocean_droplet" "web" {
+  image    = var.rocky
+  size     = var.rsize
+  count    = var.droplet_count
+  name     = "web-${count.index + 1}"
+  tags     = [digitalocean_tag.do_tag.id]
+  region   = var.region
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+  vpc_uuid = digitalocean_vpc.web_vpc.id
+  lifecycle {
+   create_before_destroy = true
+  }
+}
+
+resource "digitalocean_project_resources" "project_attach" {
+  project = data.digitalocean_project.lab_project.id
+  resources = flatten([ digitalocean_droplet.web.*.urn])
+ }
+#########################################################################
+
+#add balancer
+resource "digitalocean_loadbalancer" "public" {
+ name = "loadbalancer-assign01"
+ region = var.region
+
+forwarding_rule {
+   entry_port = 80
+   entry_protocol = "http"
+
+   target_port = 80
+   target_protocol = "http"
+ }
+
+ healthcheck {
+  port      = 22
+  protocol  = "tcp"
+ }
+
+ droplet_tag = var.tag 
+ vpc_uuid = digitalocean_vpc.web_vpc.id
+}
+
+
+
+resource "digitalocean_firewall" "web_dp_firewall" {
+
+    # The name we give our firewall for ease of use                            #    
+    name = "web-firewall"
+
+    # The droplets to apply this firewall to                                   #
+    droplet_ids = digitalocean_droplet.web.*.id
+
+    # Internal VPC Rules. We have to let ourselves talk to each other
+    inbound_rule {
+        protocol = "tcp"
+        port_range = "1-65535"
+        source_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    inbound_rule {
+        protocol = "udp"
+        port_range = "1-65535"
+        source_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    inbound_rule {
+        protocol = "icmp"
+        source_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "udp"
+        port_range = "1-65535"
+        destination_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "1-65535"
+        destination_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    outbound_rule {
+        protocol = "icmp"
+        destination_addresses = [digitalocean_vpc.web_vpc.ip_range]
+    }
+
+    # Selective Outbound Traffic Rules
+
+    # HTTP
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "80"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    # HTTPS
+    outbound_rule {
+        protocol = "tcp"
+        port_range = "443"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+    # ICMP (Ping)
+    outbound_rule {
+        protocol              = "icmp"
+        destination_addresses = ["0.0.0.0/0", "::/0"]
+    }
+
+}
+    output "server_ip" {
+  description = "server ip address:"  
+  value = digitalocean_droplet.web.*.ipv4_address
+ }
+
+# output "vpc_id" {  
+#   description = "ID of project VPC:"  
+#   value     = digitalocean_vpc.web_vpc.id
+#   }
+
+# output "lb_url" {
+#     description = "URL of load balancer:"
+#     value     = "loadbalancer-assign01"
+#   }
+      
+# output "web_server_count" {
+#     description = "Number of web servers provisioned"
+#     value       = ${count.index}#var.droplet_count
+#   }
---
+++ dev/variables.tf
@@ -0,0 +1,64 @@
+# The API token
+variable "do_token" {}
+
+#set the default region to sfo3
+variable "region" {
+ type = string
+default = "sfo3"
+}
+
+#set the default droplet count
+variable "droplet_count" {
+ type = number
+ default = 1 
+}
+
+#set ssh key name
+variable "ssh_key" {
+ type = string
+ default = "lab4Rocky"
+}
+
+#set project name
+variable "project" {
+ type = string
+ default = "4640-labs"
+}
+
+#set tag name
+variable "tag" {
+ type = string
+ default = "tag7assign01"
+}
+
+#set vpc name
+variable "vpc" {
+ type = string
+ default = "vpcassign01"
+}
+
+#set rockylinux version
+variable "rocky" {
+ type = string
+ default = "rockylinux-9-x64"
+}
+
+
+#set rockylinux size
+variable "rsize" {
+ type = string
+ default = "s-1vcpu-512mb-10gb"
+}
+
+#set ubuntu version
+variable "ubuntu" {
+ type = string
+ default = "ubuntu-22-04-x64"
+}
+
+#set rockylinux size
+variable "usize" {
+ type = string
+ default = "s-1vcpu-1gb"
+}
+
